0fdad1b9702a79276327c66cba41b07b4cca1f17
gisellyreis <gisellyar@ufba.br>
2021-06-24 00:13:49 -0300

    NLW - Aula 02 - Maximum Speed - Tela de autenticação, - Tela de criação de sala - Navegação - Autenticação com firebase (Google)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { BrowserRouter, Route } from "react-router-dom";
+import { AuthContextProvider } from "./contexts/AuthContext";
+
+
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <BrowserRouter>
+        <AuthContextProvider>
+          <Route exact path="/" component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+        </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
